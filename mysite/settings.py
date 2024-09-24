c01646255f44feb18f37ab0eabaa09330ee06571
shivangi <shivangi@gmail.com>
2024-09-24 15:04:55 +0530

    change requirements filee

--- mysite/settings.py
+++ mysite/settings.py
@@ -14,6 +14,7 @@ from pathlib import Path
 import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+ALLOWED_HOSTS = ['render-zp3w.onrender.com']
 
 
 # Quick-start development settings - unsuitable for production
